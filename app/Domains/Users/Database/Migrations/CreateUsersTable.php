1faf16db17070f48f23234057352a7cc1150f2de
Diego Hernandes <diego@hernandev.com>
2016-06-04 15:53:30 -0300

    commit inicial

---
+++ app/Domains/Users/Database/Migrations/CreateUsersTable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Domains\Users\Database\Migrations;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * @var \Illuminate\Database\Schema\Builder
+     */
+    protected $schema;
+
+    /**
+     * Migration constructor.
+     */
+    public function __construct()
+    {
+        $this->schema = app('db')->connection()->getSchemaBuilder();
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('users');
+    }
+}
